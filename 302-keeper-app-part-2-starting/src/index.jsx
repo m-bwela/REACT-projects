558169c90d4dd28ca49ba758ab1dd66f267db43d
TyeMuzan <TyeMuzan@gmail.com>
2025-06-15 15:16:53 +0300

    used props, Arrow Function and Map Function

---
+++ 302-keeper-app-part-2-starting/src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode).render(<App />);
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
