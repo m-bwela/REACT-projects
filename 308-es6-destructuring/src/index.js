fb05123630b5737ffa916c296e3414a98f123117
TyeMuzan <TyeMuzan@gmail.com>
2025-06-25 14:01:37 +0300

    destructuring Arrays&Objects

---
+++ 308-es6-destructuring/src/index.js
@@ -0,0 +1,51 @@
+//import animals /*{ useAnimals }*/ from "./data";
+
+//Destructuring Arrays
+ //console.log(animals);
+// const [cat, dog] = animals;
+// console.log(cat);
+
+//Destructuring Objects
+// const {name: catName, sound: catSound} = cat;    /*or cat.name and cat.sound*/
+// console.log(catSound);
+/*alternatively for changing names
+const { name: catName, sound: catSound } */
+//const {name, sound, feedingRequirements: {food, water} } = cat;
+//console.log(food);
+
+// const [cat, dog] = animals;
+// console.log(useAnimals(cat));
+// const [animal, makeSound] = useAnimals(cat);
+// console.log(animal);
+// makeSound();
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import { createRoot } from "react-dom/client";
+import cars from "./practice";
+
+
+const [honda, tesla] = cars;
+console.log(honda);
+
+
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode).render(
+    <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>
+);
